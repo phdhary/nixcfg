c23fe01ba91029f2a74ab5973243118d1a0a0fda
Hary Prihadi <phdhary@gmail.com>
2023-09-20 22:59:13 +0700

    fix: kitty

--- home/terminal-emulators/default.nix
+++ home/terminal-emulators/default.nix
@@ -3,6 +3,9 @@
   thisPath = "/home/terminal-emulators/";
 in {
   home.file =
-    makeConfigSymlink thisPath "kitty/"
+    # makeConfigSymlink thisPath "kitty/"
+    makeConfigSymlink thisPath "kitty/kitty.conf"
+    // makeConfigSymlink thisPath "kitty/scrollback-pager/init.lua"
+    // makeConfigSymlink thisPath "kitty/themes/"
     // makeConfigSymlink thisPath "wezterm/";
 }
