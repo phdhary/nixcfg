fd0745a0f5397039376e0d649d8ba378d46cac9b
Hary Prihadi <phdhary@gmail.com>
2023-10-05 13:53:15 +0700

    feat(batresudah): ditch `while` and `sleep` & use systemd timer

--- modules/services/batresudah/default.nix
+++ modules/services/batresudah/default.nix
@@ -10,8 +10,18 @@ in {
 
   config = mkIf cfg.enable {
     systemd.user.services."${name}" = {
-      Unit = { Description = "battery notifications"; };
+      Unit = { Description = "a battery notifications through ntfy"; };
       Service = { ExecStart = "${packages.batresudah}/bin/${name}"; };
+    };
+
+    systemd.user.timers."${name}" = {
+      Unit = { Description = "batresudah.service timer"; };
+      Timer = {
+        OnBootSec = "0min";
+        OnCalendar = "*:5/5"; # start at minute 5, repeat every 5 minute.
+        # OnCalendar = "*:*:00"; # every minute
+        Unit = "batresudah.service";
+      };
       Install = { WantedBy = [ "default.target" ]; };
     };
   };
--- packages/batresudah/batresudah.sh
+++ packages/batresudah/batresudah.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 lowerThreshold=20;
 upperThreshold=90;
+
 notipai() {
   curl \
     -H "Title: $USER@$(hostname) " \
@@ -9,15 +10,16 @@ notipai() {
     -d "$1 [ $2% ]" \
     ntfy.sh/phdhary_sub
 }
-printf "batresudah started"
-while true; do
-  batteryLevel=$(@upower@ -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep -P -o '[0-9]+(?=%)');
-  state=$(@upower@ -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk -F ' ' '{print $2}')
-  if [ $batteryLevel -lt $lowerThreshold ] && [ $state == "discharging" ]; then
-    notipai "I need more power" $batteryLevel
-  fi
-  if [ $batteryLevel -gt $upperThreshold ] && [ $state == "charging" ]; then
-    notipai "Unplug me" $batteryLevel
-  fi
-  sleep 5m
-done
+
+batteryLevel=$(@upower@ -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep -P -o '[0-9]+(?=%)');
+state=$(@upower@ -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk -F ' ' '{print $2}')
+
+# shellcheck disable=2086,3014
+if [ $batteryLevel -lt $lowerThreshold ] && [ $state == "discharging" ]; then
+  notipai "I need more power" $batteryLevel
+fi
+
+# shellcheck disable=2086,3014
+if [ $batteryLevel -gt $upperThreshold ] && [ $state == "charging" ]; then
+  notipai "Unplug me" $batteryLevel
+fi
