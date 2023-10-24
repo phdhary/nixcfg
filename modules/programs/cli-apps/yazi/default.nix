7ce0e24b3f718182b72ebcf378bc3628710930e5
Hary Prihadi <phdhary@gmail.com>
2023-10-24 14:21:38 +0700

    feat: tidy up yazi

--- modules/programs/cli-apps/yazi/default.nix
+++ modules/programs/cli-apps/yazi/default.nix
@@ -1,25 +1,29 @@
 { config, namespace, lib, pkgs, ... }:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkOption mkIf types;
   inherit (pkgs.lib) mkConfigSymlinkFromList;
+  inherit (pkgs) writeShellScriptBin symlinkJoin;
+  inherit (builtins) readFile;
+  yz = writeShellScriptBin "yz" (readFile ./yz.sh);
   cfg = config.${namespace}.programs.cli-apps.yazi;
-  package = pkgs.unstable-fdd89.yazi;
+  yazi = pkgs.unstable-fdd89.yazi;
+  ya = let 
+  in symlinkJoin {
+    name = "yazi";
+    paths = [ yz yazi ];
+  };
+
 in {
   options.${namespace}.programs.cli-apps.yazi = {
     enable = mkEnableOption "yazi";
+    package = mkOption {
+      type = types.package;
+      default = ya;
+    };
   };
 
   config = mkIf cfg.enable {
-    home.packages = [
-      (let
-        inherit (pkgs) writeShellScriptBin symlinkJoin;
-        inherit (builtins) readFile;
-        yz = writeShellScriptBin "yz" (readFile ./yz.sh);
-      in symlinkJoin {
-        name = "yazi";
-        paths = [ yz package ];
-      })
-    ];
+    home.packages = [ cfg.package ];
     home.file = mkConfigSymlinkFromList {
       relativePath = "modules/programs/cli-apps";
       paths = [ "yazi/keymap.toml" "yazi/yazi.toml" "yazi/theme.toml" ];
@@ -29,7 +33,7 @@ in {
       comment =
         "Blazing fast terminal file manager written in Rust, based on async I/O.";
       icon = "utilities-terminal";
-      exec = "${package}/bin/yazi";
+      exec = "${yazi}/bin/yazi";
       categories = [ "ConsoleOnly" "System" "FileTools" "FileManager" ];
       terminal = true;
     };
